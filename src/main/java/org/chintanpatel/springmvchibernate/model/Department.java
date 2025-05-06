48a6b27ddf20bb957a23ff0496049826d337fef9
chintanpatel <chintanpatel9065@gmail.com>
2025-05-06 10:15:50 +0530

    Add Department entity and configuration for PostgreSQL
    Introduced the `Department` entity with JPA annotations for persistence. Configured IntelliJ project settings to include a PostgreSQL datasource and adjusted inspection profiles for unused code warnings. This setup supports database integration and code quality improvements.

---
+++ src/main/java/org/chintanpatel/springmvchibernate/model/Department.java
@@ -0,0 +1,42 @@
+package org.chintanpatel.springmvchibernate.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "tbl_department")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "department_id", nullable = false)
+    private Long departmentId;
+
+    @NotEmpty(message = "NotEmpty.department.departmentName")
+    @Column(name = "department_name", nullable = false)
+    private String departmentName;
+
+    public Department() {
+    }
+
+    public Department(Long departmentId, String departmentName) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+}
